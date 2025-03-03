8a137f8565599c165d6f5c6f1e6d971660962603
Hitesh Yadav <hiteshy468@gmail.com>
2025-03-03 22:06:03 +0530

    add apk file

--- client/src/Components/AppComponent/AppComponent.jsx
+++ client/src/Components/AppComponent/AppComponent.jsx
@@ -9,18 +9,21 @@ function AppComponent() {
                     <h2 className=' text-4xl font-bold mb-4'>Download App For Best Experience</h2>
                     <p className="text-gray-600 mb-4">Download the Olyox app for the best experience, offering tailored solutions for both users and vendors alike.</p>
                     <div className='flex flex-col sm:flex-row items-center justify-center gap-3'>
-                        <Link
-                            to={''}
+                        <a
+                            href={'https://drive.google.com/file/d/1hRmNgJuy9sMs4sGibVWwbVT9TZzrbvfh/view?usp=sharing'}
+                            download
+                            target='_blank'
+                            filename='Olyox.apk'
                             className='w-full sm:w-auto text-center font-bold text-[#A91E1B] bg-white py-3 px-6 border border-[#A91E1B] rounded-xl hover:text-white hover:bg-[#A91E1B]'
                         >
                             Download User App
-                        </Link>
-                        <Link
-                            to={''}
+                        </a>
+                        <a
+                            href={''}
                             className='w-full sm:w-auto text-center font-bold text-[#A91E1B] bg-white py-3 px-6 border border-[#A91E1B] rounded-xl hover:text-white hover:bg-[#A91E1B]'
                         >
                             Download Vendor App
-                        </Link>
+                        </a>
                     </div>
 
                 </div>
