8278864c52019a39e47e07c8f482657aa8297c09
Coderhitesh <hiteshy468@gmail.com>
2025-03-06 12:26:47 +0530

    add

--- admin/src/routes.js
+++ admin/src/routes.js
@@ -49,6 +49,7 @@ const Dashboard = React.lazy(() => import('./views/dashboard/Dashboard'))
 const Colors = React.lazy(() => import('./views/theme/colors/Colors'))
 const Typography = React.lazy(() => import('./views/theme/typography/Typography'))
 
+
 // Base
 const Accordion = React.lazy(() => import('./views/base/accordion/Accordion'))
 const Breadcrumbs = React.lazy(() => import('./views/base/breadcrumbs/Breadcrumbs'))
