85a8ec280976e172b7b241a3ff09af743632464b
Anish jha <122715402+anish-stack@users.noreply.github.com>
2025-04-30 13:05:44 +0530

    Update Login.jsx

--- client/src/Pages/Auth/Login.jsx
+++ client/src/Pages/Auth/Login.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
 import toast from 'react-hot-toast';
-
+axios.defaults.withCredentials = true;
 function Login() {
   const location = new URLSearchParams(window.location.search);
   const bh = location.get("bh");
