7beb549b5f06f565706425b0657b8e99a076efb2
Anish jha <122715402+anish-stack@users.noreply.github.com>
2025-05-01 11:06:11 +0530

    Update ContactPage.jsx.jsx

--- client/src/Pages/Contact/ContactPage.jsx.jsx
+++ client/src/Pages/Contact/ContactPage.jsx.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ContactHero from './ContactHero';
 import ContactForm from './ContactForm';
 import ContactInfo from './ContactInfo';
 import ContactMap from './ContactMap';
 
 const ContactPage = () => {
+  useEffect(() => {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      })
+    }, [])
   return (
     <div className="min-h-screen">
       <ContactHero />
